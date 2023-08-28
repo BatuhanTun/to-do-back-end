b09cb074e678a6dabed87aad10fc135b943c5ba6
Batuhan TUNÇAY <batuhantunayy@hotmail.com>
2023-08-29 00:44:31 +0300

    delete düzenlemeler

--- delete_to_do.js
+++ delete_to_do.js
@@ -3,8 +3,8 @@ const docClient = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
   try {
-    const data = await docClient.delete(event).promise()
-   return { body: 'Successfully deleted item!' }
+    await docClient.delete(event).promise()
+    return true;
   } catch (err) {
     return { error: err };
   }
